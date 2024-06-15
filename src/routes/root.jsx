3c1d42a4b115a7e9a006b50a1856597a2fa3e754
hudsonviana <hudson1206@gmail.com>
2024-06-15 16:07:04 -0300

    Creating Contacts

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,19 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLoaderData, Form } from 'react-router-dom';
+import { getContacts, createContact } from '../contacts';
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+export async function action() {
+  const contact = await createContact();
+  return { contact };
+}
 
 const Root = () => {
+  const { contacts } = useLoaderData();
+
   return (
     <>
       <div id="sidebar">
@@ -17,19 +30,33 @@ const Root = () => {
             <div id="search-spinner" aria-hidden hidden={true}></div>
             <div className="sr-only" aria-live="polite"></div>
           </form>
-          <form method="post">
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
-          <ul>
-            <li>
-              <Link to={`/contacts/1`}>Your name</Link>
-            </li>
-            <li>
-              <Link to={`/contacts/2`}>Your friend</Link>
-            </li>
-          </ul>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{' '}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
       </div>
       <div id="detail">
