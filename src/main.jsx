3814e743448721ccb483f556ca26ae3d402d84d5
hudsonviana <hudson1206@gmail.com>
2024-06-16 00:19:36 -0300

    JSX Routes

--- src/main.jsx
+++ src/main.jsx
@@ -55,3 +55,47 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+// JSX Routes
+
+/**
+
+  import {
+    createRoutesFromElements,
+    createBrowserRouter,
+    Route,
+  } from "react-router-dom";
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route
+        path="/"
+        element={<Root />}
+        loader={rootLoader}
+        action={rootAction}
+        errorElement={<ErrorPage />}
+      >
+        <Route errorElement={<ErrorPage />}>
+          <Route index element={<Index />} />
+          <Route
+            path="contacts/:contactId"
+            element={<Contact />}
+            loader={contactLoader}
+            action={contactAction}
+          />
+          <Route
+            path="contacts/:contactId/edit"
+            element={<EditContact />}
+            loader={contactLoader}
+            action={editAction}
+          />
+          <Route
+            path="contacts/:contactId/destroy"
+            action={destroyAction}
+          />
+        </Route>
+      </Route>
+    )
+  );
+
+ */
